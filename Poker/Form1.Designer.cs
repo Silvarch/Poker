67228774b14fd3bcb0e392c50975ca339a12d8fa
Logan <logan.larocque@snhu.edu>
2022-02-11 17:10:28 -0800

    Created Card class which has methods to creat a card when called. see notes in program for more details.
    Also created Deck class which assembles a deck of 52 cards by creating an array and calling the 'CreateCard' method within the Card class.
    
    I added a text box and button to the form for testing purposes. currently that button fills the textbox with all of the cards in the deck in order to verify that the deck is filled with the appropriate amount of cards. this may be useful in verifying that the deck is shuffled once that method is created.
    
    I plan to add graphics for the deck and cards relatively soon.

--- Poker/Form1.Designer.cs
+++ Poker/Form1.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.DeckTest = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TestButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // DeckTest
+            // 
+            this.DeckTest.Location = new System.Drawing.Point(692, 12);
+            this.DeckTest.Multiline = true;
+            this.DeckTest.Name = "DeckTest";
+            this.DeckTest.Size = new System.Drawing.Size(244, 890);
+            this.DeckTest.TabIndex = 0;
+            this.DeckTest.TextChanged += new System.EventHandler(this.DeckTest_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(524, 288);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Cards in deck:";
+            // 
+            // TestButton
+            // 
+            this.TestButton.Location = new System.Drawing.Point(572, 341);
+            this.TestButton.Name = "TestButton";
+            this.TestButton.Size = new System.Drawing.Size(75, 23);
+            this.TestButton.TabIndex = 2;
+            this.TestButton.Text = "Test";
+            this.TestButton.UseVisualStyleBackColor = true;
+            this.TestButton.Click += new System.EventHandler(this.TestButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(939, 914);
+            this.Controls.Add(this.TestButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DeckTest);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox DeckTest;
+        private Label label1;
+        private Button TestButton;
     }
 }
