d610dc62c6e7b5332c2cefeb21b5bbac11f46434
Logan <logan.larocque@snhu.edu>
2022-02-14 16:46:56 -0800

    Added graphics for each card, attempted to work on suffle method but did nto finish as I have class soon. Will finish shuffle method and dealing method as soon as I can

--- Poker/Deck.cs
+++ Poker/Deck.cs
@@ -8,8 +8,10 @@ namespace Poker
 {
     internal class Deck
     {
-        String[] NewDeck = new String[52]; // this array holds the 52 card deck with each element being a card.
+        public String[] NewDeck = new String[52]; // this array holds the 52 card deck with each element being a card.
         Card c1 = new Card();
+        
+        
 
         public string SetDeck()
         {
@@ -35,6 +37,141 @@ namespace Poker
 
             return result;
 
+        }
+        public string BeginShuffle()
+        {
+            string result = Shuffle(NewDeck);
+
+            return result;
+
+        }
+        
+        static string Shuffle(string[] NewDeck)
+        {
+            Random random = new Random();
+            NewDeck = NewDeck.OrderBy(x => random.Next()).ToArray();
+
+            String seperator = "\n";
+            string result = "";
+            result += String.Join(seperator, NewDeck);
+
+            return result;
+
+
+        }   
+        
+        public int Draw(int DrawNumber)
+        {
+            int card = 0;
+            switch (DrawNumber)
+            {
+                case 0:
+                    card = 0; break;
+                case 1:
+                    card = 1; break;
+                case 2:
+                    card = 2; break;
+                case 3:
+                    card = 3; break;
+                case 4:
+                    card = 4; break;
+                case 5:
+                    card = 5; break;
+                case 6:
+                    card = 6; break;
+                case 7:
+                    card = 7; break;
+                case 8:
+                    card = 8; break;
+                case 9:
+                    card = 9; break;
+                case 10:
+                    card = 10; break;
+                case 11:
+                    card = 11; break;
+                case 12:
+                    card = 12; break;
+                case 13:
+                    card = 13; break;
+                case 14:
+                    card = 14; break;
+                case 15:
+                    card = 15; break;
+                case 16:
+                    card = 16; break;
+                case 17:
+                    card = 17; break;
+                case 18:
+                    card = 18; break;
+                case 19:
+                    card = 19; break;
+                case 20:
+                    card = 20; break;
+                case 21:
+                    card = 21; break;
+                case 22:
+                    card = 22; break;
+                case 23:
+                    card = 23; break;
+                case 24:
+                    card = 24; break;
+                case 25:
+                    card = 25; break;
+                case 26:
+                    card = 26; break;
+                case 27:
+                    card = 27; break;
+                case 28:
+                    card = 28; break;
+                case 29:
+                    card = 29; break;
+                case 30:
+                    card = 30; break;
+                case 31:
+                    card = 31; break;
+                case 32:
+                    card = 32; break;
+                case 33:
+                    card = 33; break;
+                case 34:
+                    card = 34; break;
+                case 35:
+                    card = 35; break;
+                case 36:
+                    card = 36; break;
+                case 37:
+                    card = 37; break;
+                case 38:
+                    card = 38; break;
+                case 39:
+                    card = 39; break;
+                case 40:
+                    card = 40; break;
+                case 41:
+                    card = 41; break;
+                case 42:
+                    card = 42; break;
+                case 43:
+                    card = 43; break;
+                case 44:
+                    card = 44; break;
+                case 45:
+                    card = 45; break;
+                case 46:
+                    card = 46; break;
+                case 47:
+                    card = 47; break;
+                case 48:
+                    card = 48; break;
+                case 49:
+                    card = 49; break;
+                case 50:
+                    card = 50; break;
+                case 51:
+                    card = 51; break;
+
+            }
+            return card;
         }
     }
 }
--- Poker/Form1.cs
+++ Poker/Form1.cs
@@ -3,6 +3,8 @@ namespace Poker
     public partial class Form1 : Form
     {
         Deck d1 = new Deck();
+        int DrawNumber = 0; //this sets the current draw number and should be updated for each draw and reset if a new game is started
+        int turn = 0;
         public Form1()
         {
             InitializeComponent();
@@ -14,11 +16,113 @@ namespace Poker
         }
 
 
-        //outputs deck contents to DeckTest textbox
+        //shuffles deck and outputs deck contents to DeckTest textbox (testing purposes)
         private void TestButton_Click(object sender, EventArgs e)
         {
-            string test = d1.SetDeck();
+            d1.SetDeck();
+            string test = d1.BeginShuffle();
             DeckTest.Text = test;
+
+        }
+
+        private void ButtonDeck_Click(object sender, EventArgs e)
+        {
+            DrawNumber++;
+            int CardDraw = d1.Draw(DrawNumber); //because draw number is incremented though each iteration, the card darawn will always be the next in the arrays order
+
+            string r1 = "Ace";
+            string r2 = "Two";
+            string r3 = "Three";
+            string r4 = "Four";
+            string r5 = "Five";
+            string r6 = "Six";
+            string r7 = "Seven";
+            string r8 = "Eight";
+            string r9 = "Nine";
+            string r10 = "Ten";
+            string r11 = "Jack";
+            string r12 = "Queen";
+            string r13 = "King";
+            string s1 = "Hearts";
+            string s2 = "Diamonds";
+            string s3 = "Clubs";
+            string s4 = "Spades";
+
+            turn = 0; //remove after testing done
+
+            //The following if statements check what turn it is along with the value of the card being drawn via the NewDeck Array
+            if (turn == 0 && d1.NewDeck[CardDraw].Contains(r11) && d1.NewDeck[CardDraw].Contains(s1))
+            {
+                Player1Card1.Image = Poker.Properties.Resources.JackHearts;
+                turn++;
+            }
+            if (turn == 0 && d1.NewDeck[CardDraw].Contains(r11) && d1.NewDeck[CardDraw].Contains(s2))
+            {
+                Player1Card1.Image = Poker.Properties.Resources.JackDiamonds;
+                turn++;
+            }
+            if (turn == 0 && d1.NewDeck[CardDraw].Contains(r11) && d1.NewDeck[CardDraw].Contains(s3))
+            {
+                Player1Card1.Image = Poker.Properties.Resources.JackClubs;
+                turn++;
+            }
+            if (turn == 0 && d1.NewDeck[CardDraw].Contains(r11) && d1.NewDeck[CardDraw].Contains(s4))
+            {
+                Player1Card1.Image = Poker.Properties.Resources.JackSpades;
+                turn++;
+            }
+            if (turn == 0 && d1.NewDeck[CardDraw].Contains(r12) && d1.NewDeck[CardDraw].Contains(s1))
+            {
+                Player1Card1.Image = Poker.Properties.Resources.QueenHearts;
+                turn++;
+            }
+            if (turn == 0 && d1.NewDeck[CardDraw].Contains(r12) && d1.NewDeck[CardDraw].Contains(s2))
+            {
+                Player1Card1.Image = Poker.Properties.Resources.QueenDiamonds;
+                turn++;
+            }
+            if (turn == 0 && d1.NewDeck[CardDraw].Contains(r12) && d1.NewDeck[CardDraw].Contains(s3))
+            {
+                Player1Card1.Image = Poker.Properties.Resources.QueenClubs;
+                turn++;
+            }
+            if (turn == 0 && d1.NewDeck[CardDraw].Contains(r12) && d1.NewDeck[CardDraw].Contains(s4))
+            {
+                Player1Card1.Image = Poker.Properties.Resources.QueenSpades;
+                turn++;
+            }
+            if (turn == 0 && d1.NewDeck[CardDraw].Contains(r13) && d1.NewDeck[CardDraw].Contains(s1))
+            {
+                Player1Card1.Image = Poker.Properties.Resources.KingHearts;
+                turn++;
+            }
+            if (turn == 0 && d1.NewDeck[CardDraw].Contains(r13) && d1.NewDeck[CardDraw].Contains(s2))
+            {
+                Player1Card1.Image = Poker.Properties.Resources.KingDiamonds;
+                turn++;
+            }
+            if (turn == 0 && d1.NewDeck[CardDraw].Contains(r13) && d1.NewDeck[CardDraw].Contains(s3))
+            {
+                Player1Card1.Image = Poker.Properties.Resources.KingClubs;
+                turn++;
+            }
+            if (turn == 0 && d1.NewDeck[CardDraw].Contains(r13) && d1.NewDeck[CardDraw].Contains(s4))
+            {
+                Player1Card1.Image = Poker.Properties.Resources.KingSpades;
+                turn++;
+            }
+
+        }
+
+        private void Shuffle_Click(object sender, EventArgs e)
+        {
+            d1.BeginShuffle();
+            
+        }
+
+        private void test2_Click(object sender, EventArgs e)
+        {
+            DeckTest.Text = d1.NewDeck[0];
         }
     }
 }
